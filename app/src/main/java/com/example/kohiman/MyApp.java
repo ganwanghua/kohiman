ca80846491b07de555db682f61479e1fba21cee1
ganwanghua <1345895582@qq.com>
2021-04-13 13:37:14 +0800

    first commit

---
+++ app/src/main/java/com/example/kohiman/MyApp.java
@@ -0,0 +1,21 @@
+package com.example.kohiman;
+
+import android.app.Application;
+
+import com.pedaily.yc.ycdialoglib.toast.ToastUtils;
+
+
+public class MyApp extends Application {
+    private static MyApp sInstance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        sInstance = this;
+        ToastUtils.init(this);
+    }
+
+    public static MyApp getInstance() {
+        return sInstance;
+    }
+}
